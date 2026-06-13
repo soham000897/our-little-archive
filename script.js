45c4c16d4e0ad07c6426e49bc196fd1762e52060
Soham debnath <Freedebnath6@gmail.com>
2026-06-13 15:44:45 +0530

    Add star animation and typing effect in script.js

--- script.js
+++ script.js
@@ -1,1 +1,100 @@
+/* stars */
 
+for(let i=0;i<90;i++){
+
+let s=document.createElement("div");
+
+s.className="star";
+
+s.style.left=Math.random()*100+"vw";
+
+s.style.top=Math.random()*100+"vh";
+
+s.style.animationDelay=Math.random()*4+"s";
+
+document.body.appendChild(s);
+
+}
+
+
+/* typing lines */
+
+let lines=[
+
+"Searching through forgotten conversations...",
+
+"Collecting moments worth remembering...",
+
+"Finding laughter hidden in old chats...",
+
+"Recovering memories created over time...",
+
+"Detecting a friendship that still matters...",
+
+"Someone important has been found...",
+
+"Hello Mimo 💙"
+
+];
+
+let box=document.getElementById("text");
+
+let fill=document.getElementById("fill");
+
+let i=0;
+
+
+function write(line,done){
+
+let j=0;
+
+let div=document.createElement("div");
+
+box.appendChild(div);
+
+let timer=setInterval(()=>{
+
+div.innerHTML=line.substring(0,j)+"<span class='cursor'>|</span>";
+
+j++;
+
+if(j>line.length){
+
+clearInterval(timer);
+
+if(line.includes("Hello")){
+
+div.classList.add("final");
+
+}
+
+div.innerHTML=line;
+
+done();
+
+}
+
+},32);
+
+}
+
+
+function start(){
+
+if(i<lines.length){
+
+write(lines[i],()=>{
+
+fill.style.width=((i+1)*14)+"%";
+
+i++;
+
+setTimeout(start,650);
+
+});
+
+}
+
+}
+
+start();
